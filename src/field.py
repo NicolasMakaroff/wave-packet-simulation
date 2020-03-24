1d4b0c6f24a379aea95df7ec2532a80b4ce90767
Nicolas Makaroff <nicolas.makaroff@ensiie.fr>
2020-03-24 18:11:55 +0100

    add function to read an image as a potential
    some writing have been also made

--- src/field.py
+++ src/field.py
@@ -1,6 +1,6 @@
 import numpy as np
+from matplotlib.image import imread
 
-start_x_, double start_y_, int size_, int spatial_step_, int time_step_, arma::cx_mat potential_, arma::cx_mat initial_state_
 class Field:
         """
         Define a class for a field generator.
@@ -17,7 +17,7 @@ class Field:
         """
         def __init__(self, _start_x_, _start_y_, _size_, _spatial_step_, _time_step_):
                 self._start_x_ = _start_x_
-                self._start_Y_ = _start_y_
+                self._start_y_ = _start_y_
                 self._size_ = _size_
                 self._spatial_step_ = _spatial_step_
                 self._time_step_ = _time_step_
@@ -25,20 +25,20 @@ class Field:
                 self._initial_state_ = np.ascontiguousarray(np.zeros([self._size_,self._size_]),dtype=np.complex) 
 
         def _setPotential_(self,_potential_array):
-                if array.shape == (self._size_,self._size_):
+                if _potential_array.shape == (self._size_,self._size_):
                         self._potential_ = _potential_array
                         return True
                 else:
                         raise ValueError(
-                '{} array object must be 2D array of the size _size_. Given size: {} '.format(self.__class__.__name__,array.shape))
+                '{} array object must be 2D array of the size _size_. Given size: {} '.format(self.__class__.__name__,_potential_array.shape))
         
         def _setInitialState_(self,_initial_state_array):
-                if array.shape == (self._size_,self._size_):
+                if _initial_state_array.shape == (self._size_,self._size_):
                         self._potential_ = _initial_state_array
                         return True
                 else:
                         raise ValueError(
-                '{} array object must be 2D array of the size _size_. Given size: {} '.format(self.__class__.__name__,array.shape))
+                '{} array object must be 2D array of the size _size_. Given size: {} '.format(self.__class__.__name__,_initial_state_array.shape))
 
         def _calc_initial_state(self,func):
                 """
@@ -53,15 +53,31 @@ class Field:
                                 new_j_ = self._start_y_ + self._spatial_step_ * j 
                                 self._initial_state_[i][j] = func(new_i_, new_j_)
         
-        def _calc_initial_gaussian_state_(self,mean_x_, mean_y_, sd_):     
+        def _calc_initial_gaussian_state_(self,mean_x_, mean_y_, sd_): 
+                """
+                Vectorized function.
+                _size_ = 10000
+                Without loops => 12.81 s
+                with loops => 223.5 s
+                """    
                 discret_step_ = np.linspace(start = 0,stop = self._size_,num=(self._size_))
                 new_i_ = self._start_x_ + self._spatial_step_ * discret_step_
                 new_j_ = self._start_y_ + self._spatial_step_ * discret_step_
                 xx, yy = np.meshgrid(new_i_,new_j_)
                 X = (xx - mean_x_)
-                Y = (xx - mean_y_)
+                Y = (yy - mean_y_)
                 self._initial_state_ = np.exp(-1 * (np.square(X) + np.square(Y)) / sd_**2 ) 
 
+        def _from_image_(self,image):
+                img = imread(image)
+                if img.shape == (self._size_,self._size_,3):
+                        img = np.ascontiguousarray(img,dtype=np.complex32)
+                        self._setPotential_(img)
+                else:
+                        raise ValueError(
+                '{} array object must be 2D array of the size _size_. Given size: {} '.format(self.__class__.__name__,img.shape))
+        
+        
         
 
         
